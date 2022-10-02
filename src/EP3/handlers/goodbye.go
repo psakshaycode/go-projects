7f988cd87c8cfcdfa1ff342def73a6bcd43763a8
psakshaycode <psakshay16121997@gmail.com>
2022-10-02 14:00:58 +0530

    EP3

---
+++ src/EP3/handlers/goodbye.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Goodbye struct {
+	l *log.Logger
+}
+
+func NewGoodbye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
+func (g *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	g.l.Println("Goodbye")
+	fmt.Fprintln(rw, "Goodbye")
+}
